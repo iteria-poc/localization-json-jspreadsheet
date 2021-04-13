72a3d4e5a1f0513098b6a3c3ee8ffc98023cbd51
maheechy77 <maheechy96@gmail.com>
2021-04-14 00:57:48 +0600

    Rivision 3

--- src/webcomponents/helperfunctions/jsonToCol.ts
+++ src/webcomponents/helperfunctions/jsonToCol.ts
@@ -1,42 +1,31 @@
 import { JsonToCol, LanguagesInput } from "../../@types/interfaces";
 
 const mapLangToSheetColumns = (languages: LanguagesInput) => {
-  let columnTitle: Array<object> = [];
-  Object.keys(languages).map(
-    (title: string) =>
-      (columnTitle = [
-        ...columnTitle,
-        { type: "text", title: `${title}`, width: 150 },
-      ])
-  );
+  let columnTitle: Array<object> = Object.keys(
+    languages
+  ).map((title: string) => ({ type: "text", title: `${title}`, width: 150 }));
   return columnTitle;
 };
 
 const setKeys = (languages: LanguagesInput): Array<string> => {
-  let languagesKeys: Array<string> = [];
-  Object.keys(languages).map(
-    (titles) => (languagesKeys = [...languagesKeys, titles])
-  );
-  const keyValue = Object.keys(languages[languagesKeys[0]]);
-  return keyValue;
+  const keyValue = new Set();
+  const reducer = (accumulator: object, currentValue: object) =>
+    keyValue.add(Object.keys(currentValue));
+  Object.keys(languages)
+    .map((titles) => languages[titles])
+    .reduce(reducer);
+  return keyValue.values().next().value;
 };
 
 const mapLangToSheetData = (languages: LanguagesInput) => {
   let rowsData: Array<object> = [];
-  let keys: Array<string> = setKeys(languages);
-  let tempRows: any = [];
   let tempCol: Array<object> = [];
-
-  Object.keys(languages).map(
-    (titles) => (tempRows = [...tempRows, languages[titles]])
+  const tempRows: any = Object.keys(languages).map(
+    (titles) => languages[titles]
   );
-  Object.keys(keys).map((key: any) => {    
-    Object.keys(tempRows).map((row:any) => {
-      Object.keys(tempRows[row]).map((rowKey:string) => {
-        if (rowKey === keys[key]) {
-          tempCol = [...tempCol, tempRows[row][rowKey]];
-        }
-      });
+  Object.keys(setKeys(languages)).map((key: string | any) => {
+    Object.keys(tempRows).map((row: string | number) => {
+      tempCol = [...tempCol, tempRows[row][setKeys(languages)[key]]];
     });
     rowsData = [...rowsData, tempCol];
     tempCol = [];
